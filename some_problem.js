714302a4435a81ff35fcebd49d76f65af4de9266
SanimSRk <hasansanim562@gmail.com>
2024-02-07 11:25:23 +0600

    lorgest word show

---
+++ some_problem.js
@@ -0,0 +1,27 @@
+// const vawel = 'banglad desh is very popular country in the world';
+// function myFunction(vowels) {
+//   const valweld = ['a', 'e', 'i', 'o', 'u'];
+//   const vlus = vowels.split('');
+//   let sum = 0;
+//   for (let num of vlus) {
+//     if (valweld.includes(num)) {
+//       sum++;
+//     }
+//   }
+//   return sum;
+// }
+// const resutl = myFunction(vawel);
+// console.log(resutl);
+const sample = ' I am learning Programming to become a programmer';
+function searchLargestWord(word) {
+  const valus = word.split(' ');
+  let sum = '';
+  for (let i = 0; i < valus.length; i++) {
+    if (valus[i].length > sum.length) {
+      sum = valus[i];
+    }
+  }
+  return sum;
+}
+const resutled = searchLargestWord(sample);
+console.log(resutled);
